43649cbd6bf0b51a4ceea665feadda497e9df9f2
Голубев Константин Андреевич <kgolubev@cinimex.ru>
2022-08-01 13:48:10 +0300

    HW10: logic optimized

---
+++ hw10_program_optimization/stats_bench_test.go
@@ -0,0 +1,24 @@
+package hw10programoptimization
+
+import (
+	"archive/zip"
+
+	"log"
+	"testing"
+)
+
+func BenchmarkGetDomainStat(b *testing.B) {
+	reader, err := zip.OpenReader("testdata/users.dat.zip")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data, err := reader.File[0].Open()
+	defer reader.Close()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = GetDomainStat(data, "com")
+	}
+}
