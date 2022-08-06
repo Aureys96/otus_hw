b2dfb8203145d8a628200a4349e2d12c20892b36
Kostyan <konstantingolubev8@gmail.com>
2022-08-06 17:05:25 +0300

    HW11: telnet implemented

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,44 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		log.Fatalln("host and port must be provided")
+	}
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	client := NewTelnetClient(net.JoinHostPort(host, port), *timeout, os.Stdin, os.Stdout)
+
+	if err := client.Connect(); err != nil {
+		log.Fatalln("failed to connect:", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer cancel()
+
+	go func() {
+		client.Send()
+		cancel()
+	}()
+	go func() {
+		client.Receive()
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -13,9 +16,54 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (c *client) Connect() error {
+	connection, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("connection failed: %w", err)
+	}
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", c.address)
+	c.conn = connection
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *client) Close() error {
+	if c.conn != nil {
+		if err := c.conn.Close(); err != nil {
+			return fmt.Errorf("connection closure failed: %w", err)
+		}
+	}
+	return nil
+}
+
+func (c *client) Send() error {
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return fmt.Errorf("sending failed: %w", err)
+	}
+	fmt.Fprintln(os.Stderr, "...EOF")
+	return nil
+}
+
+func (c *client) Receive() error {
+	if _, err := io.Copy(c.out, c.conn); err != nil {
+		return fmt.Errorf("receiving failed: %w", err)
+	}
+	fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+	return nil
+}
